acd3e6ee5d8556d62038f0a1abef8f0dc723a978
Billy <billy.tang164@gmail.com>
2016-09-06 15:33:19 +1000

    Added the mst and half_mst heuristics

---
+++ heuristics/half_mst.py
@@ -0,0 +1,26 @@
+'''
+Create a lower bound for the problem as the MST heuristic
+is a 2 approximation
+'''
+from time import time
+from mst import mst_heuristic
+from utils.io import writer
+
+def halfmst_heuristic(n, tofile=False):
+    '''
+    :n: the network object
+    '''
+    start = time()
+    algorithm_name = 'halfmst_heuristic'
+    temp_consumption, temp_objective = mst_heuristic(n, tofile=False)
+
+    consumption = {
+        vertex: weight/2 for vertex, weight in temp_consumption.iteritems()}
+    objective = temp_objective/2
+
+    duration = time() - start
+
+    if tofile:
+        writer(n, consumption, objective, duration, algorithm_name)
+
+    return consumption, objective
---
+++ heuristics/mst.py
@@ -0,0 +1,40 @@
+'''
+The MST heuristic described by Kirousis et al
+'''
+from collections import defaultdict
+from time import time
+import networkx as nx
+
+from io import writer
+from exceptions import NotCompleteGraph
+from utils import nchoose2
+
+def mst_heuristic(n, tofile=False):
+    '''
+    :n: the network object
+    '''
+    if nchoose2(n.numvertices) != len(n.edges):
+        raise NotCompleteGraph('The graph has been preprocessed.')
+
+    else:
+        start = time()
+        algorithm_name = 'mst_heuristic'
+        consumption = defaultdict(float)
+        objective = 0
+
+        graph = nx.Graph()
+        weighted_edges = [edge.edgetuple for edge in n.edges]
+        graph.add_weighted_edges_from(weighted_edges)
+        tree = nx.minimum_spanning_tree(graph)
+
+        for vertex1, vertex2, weight in tree.edges(data=True):
+            consumption[vertex1] = max(consumption[vertex1], weight['weight'])
+            consumption[vertex2] = max(consumption[vertex2], weight['weight'])
+        objective = sum(consumption[vertex] for vertex in n.vertices)
+
+        duration = time() - start
+
+        if tofile:
+            writer(n, consumption, objective, duration, algorithm_name)
+
+        return consumption, objective
