936476e0b56617527cac13c9e54740dfe4defdcc
Billy <billy.tang164@gmail.com>
2016-09-08 16:54:10 +1000

    changed name to avoid conflict

--- custom_exceptions.py
+++ custom_exceptions.py
@@ -1,10 +1,17 @@
 '''
 Contains the following exceptions:
     - NotCompleteGraph
+    - ConsumptionNotFound
 '''
 
 class NotCompleteGraph(Exception):
     '''
     This checks if the graph contains n choose 2 edges
+    '''
+    pass
+
+class ConsumptionNotFound(Exception):
+    '''
+    If we can't warm start a vertex for a given consumption value
     '''
     pass
